8349cc06703cb4ecb52280b6858dbe8e91350e77
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:37:03 +1000

    Update splash_page.dart

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('SmartNewsDaily', style: TextStyle(fontSize: 24))),
+    );
+  }
+}
 
