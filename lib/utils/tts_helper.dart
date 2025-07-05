ebff6f5502b6ece804e992e798b5020d44ca5470
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:48:59 +1000

    Update tts_helper.dart

--- lib/utils/tts_helper.dart
+++ lib/utils/tts_helper.dart
@@ -1,1 +1,13 @@
+### tts_helper.dart
+```dart
+// Text-to-speech wrapper
+class TTSHelper {
+  final FlutterTts _tts = FlutterTts();
 
+  Future<void> speak(String text) async {
+    await _tts.setLanguage("en-US");
+    await _tts.setPitch(1.0);
+    await _tts.speak(text);
+  }
+}
+```
