06132f0a8b594f2fbdbb3503073bdcaf42cd2383
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:57:28 +1000

    Update news_details_page.dart

--- lib/pages/news_details_page.dart
+++ lib/pages/news_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../utils/tts_helper.dart';
 
 class NewsDetailsPage extends StatefulWidget {
@@ -21,11 +23,54 @@ class NewsDetailsPage extends StatefulWidget {
 
 class _NewsDetailsPageState extends State<NewsDetailsPage> {
   late final TTSHelper _ttsHelper;
+  bool isBookmarked = false;
+
+  final user = FirebaseAuth.instance.currentUser;
+  final CollectionReference favRef = FirebaseFirestore.instance.collection('favorites');
 
   @override
   void initState() {
     super.initState();
     _ttsHelper = TTSHelper();
+    _checkIfBookmarked();
+  }
+
+  void _checkIfBookmarked() async {
+    if (user == null) return;
+    final snapshot = await favRef
+        .doc(user!.uid)
+        .collection("articles")
+        .where("url", isEqualTo: widget.url)
+        .limit(1)
+        .get();
+    setState(() {
+      isBookmarked = snapshot.docs.isNotEmpty;
+    });
+  }
+
+  void _toggleBookmark() async {
+    if (user == null) return;
+
+    final articleRef = favRef.doc(user!.uid).collection("articles");
+
+    if (isBookmarked) {
+      final toRemove = await articleRef.where("url", isEqualTo: widget.url).get();
+      for (var doc in toRemove.docs) {
+        await doc.reference.delete();
+      }
+      setState(() => isBookmarked = false);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Removed from favorites")));
+    } else {
+      await articleRef.add({
+        "title": widget.title,
+        "description": widget.description,
+        "imageUrl": widget.imageUrl,
+        "url": widget.url,
+        "savedAt": DateTime.now(),
+      });
+      setState(() => isBookmarked = true);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Added to favorites")));
+    }
   }
 
   @override
@@ -43,6 +88,10 @@ class _NewsDetailsPageState extends State<NewsDetailsPage> {
           IconButton(
             icon: Icon(Icons.volume_up),
             onPressed: () => _ttsHelper.speak(widget.title + ". " + widget.description),
+          ),
+          IconButton(
+            icon: Icon(isBookmarked ? Icons.bookmark : Icons.bookmark_border),
+            onPressed: _toggleBookmark,
           ),
         ],
       ),
@@ -66,4 +115,3 @@ class _NewsDetailsPageState extends State<NewsDetailsPage> {
     );
   }
 }
-
