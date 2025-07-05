4010c8b741f18e7b18f9a589e122b35e4fd77027
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:49:36 +1000

    Update localization.dart

--- lib/utils/localization.dart
+++ lib/utils/localization.dart
@@ -1,1 +1,11 @@
+### localization.dart
+```dart
+// Language localization helper
+class LocalizationHelper {
+  static String translate(BuildContext context, String key) {
+    // Fallback dummy translation logic
+    return key;
+  }
+}
+```
 
