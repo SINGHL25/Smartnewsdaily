3aa1c842aa1520653c0704abfa31a96d4f4bb010
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:46:34 +1000

    Update settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,1 +1,12 @@
-
+### settings_page.dart
+```dart
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      body: Center(child: Text('Theme / Language / About info')),
+    );
+  }
+}
+```
