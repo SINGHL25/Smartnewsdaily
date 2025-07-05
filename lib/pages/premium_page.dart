08a734ab848feb69354b7fea39adab356a61320c
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:47:06 +1000

    Update premium_page.dart

--- lib/pages/premium_page.dart
+++ lib/pages/premium_page.dart
@@ -1,1 +1,12 @@
-
+### premium_page.dart
+```dart
+class PremiumPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Premium News")),
+      body: Center(child: Text("Subscribe to unlock premium articles.")),
+    );
+  }
+}
+```
