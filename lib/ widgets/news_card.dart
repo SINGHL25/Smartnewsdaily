683f5433d225f10c51db5d2fd7d6812779f4ff19
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:39:08 +1000

    Update news_card.dart

--- lib/ widgets/news_card.dart
+++ lib/ widgets/news_card.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NewsCard extends StatelessWidget {
+  final Map<String, dynamic> article;
+  NewsCard({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: ListTile(
+        leading: Image.network(article['image_url'], width: 100, fit: BoxFit.cover),
+        title: Text(article['title']),
+        subtitle: Text(article['description'] ?? ''),
+        trailing: IconButton(
+          icon: Icon(Icons.arrow_forward),
+          onPressed: () {
+            // Handle "Read more"
+          },
+        ),
+      ),
+    );
+  }
+}
 
