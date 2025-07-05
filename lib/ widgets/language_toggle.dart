6d67af151d5e30c640711bbc08c1dc9e4c423626
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:45:54 +1000

    Update language_toggle.dart

--- lib/ widgets/language_toggle.dart
+++ lib/ widgets/language_toggle.dart
@@ -1,1 +1,23 @@
+### language_toggle.dart
+```dart
+// Language toggle (EN / HI)
+class LanguageToggle extends StatelessWidget {
+  final Function(String) onToggle;
+  const LanguageToggle({required this.onToggle});
 
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text("EN"),
+        Switch(
+          value: Localizations.localeOf(context).languageCode == 'hi',
+          onChanged: (val) => onToggle(val ? 'hi' : 'en'),
+        ),
+        Text("हिंदी"),
+      ],
+    );
+  }
+}
+```
