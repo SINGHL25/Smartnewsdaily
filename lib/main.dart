7252060868b9b99a9ebcba1bcf0ba93e9ed2716e
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:39:52 +1000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
 
+import 'package:flutter/material.dart';
+import 'pages/splash_page.dart';
+
+void main() {
+  runApp(SmartNewsApp());
+}
+
+class SmartNewsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SmartNewsDaily',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      home: SplashPage(),
+    );
+  }
+}
