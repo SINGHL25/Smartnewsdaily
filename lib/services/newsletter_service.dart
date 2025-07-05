a277539ecaa763df18e22654bbf100cb9739afe1
Akhilesh <akhi.singh1989@gmail.com>
2025-07-06 01:48:34 +1000

    Update newsletter_service.dart

--- lib/services/newsletter_service.dart
+++ lib/services/newsletter_service.dart
@@ -1,1 +1,12 @@
-
+### newsletter_service.dart
+```dart
+// Simulated newsletter registration
+class NewsletterService {
+  Future<void> registerEmail(String email) async {
+    await FirebaseFirestore.instance.collection('newsletter').add({
+      'email': email,
+      'subscribed_at': DateTime.now()
+    });
+  }
+}
+```
